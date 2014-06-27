af6e92055904e484d5e279aa74ff87261277e387
valentinsvt <valentinsvt@hotmail.com>
2014-06-27 16:04:31 -0500

    cambio a nthTiles

--- core/src/com/svt/game/ArcadiaMain.java
+++ core/src/com/svt/game/ArcadiaMain.java
@@ -1,20 +1,43 @@
 package com.svt.game;
 
-import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-public class ArcadiaMain extends ApplicationAdapter {
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.InputProcessor;
+import com.badlogic.gdx.assets.AssetManager;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g3d.Environment;
+import com.badlogic.gdx.graphics.g3d.ModelBatch;
+import com.badlogic.gdx.graphics.g3d.ModelInstance;
+import com.badlogic.gdx.math.Matrix4;
+import com.badlogic.gdx.math.Plane;
+import com.badlogic.gdx.math.Vector3;
+import com.badlogic.gdx.utils.Array;
+
+public class ArcadiaMain implements ApplicationListener, InputProcessor{
 	SpriteBatch batch;
-	Texture img;
-	
+	public Array<Texture> textures = new Array<Texture>(); /*texturas para renderisar el mapa*/
+	OrthographicCamera cam;
+	public NthTile[][] sprites = new NthTile[10][10];
+	final Matrix4 matrix = new Matrix4();
+	final Plane xzPlane = new Plane(new Vector3(0, 1, 0), 0);
+	final Vector3 intersection = new Vector3();
+	Sprite lastSelectedTile = null;
+	final Vector3 curr = new Vector3();
+	final Vector3 last = new Vector3(-1, -1, -1);
+	final Vector3 delta = new Vector3();
+	public ModelBatch modelBatch;
+	public AssetManager assets;
+	public Array<ModelInstance> instances = new Array<ModelInstance>();
+	public Environment environment;
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
-		System.out.println("lo logre ptm");
+		textures.add(new Texture("badlogic.jpg"));
 	}
 
 	@Override
@@ -22,7 +45,79 @@ public class ArcadiaMain extends ApplicationAdapter {
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		batch.draw(img, 0, 0);
+		batch.draw(textures.get(0), 0, 0);
 		batch.end();
+	}
+
+	@Override
+	public boolean keyDown(int keycode) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean keyUp(int keycode) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean keyTyped(char character) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean touchDragged(int screenX, int screenY, int pointer) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean mouseMoved(int screenX, int screenY) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean scrolled(int amount) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void resize(int width, int height) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void pause() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void resume() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void dispose() {
+		// TODO Auto-generated method stub
+		
 	}
 }
---
+++ core/src/com/svt/game/NthTile.java
@@ -0,0 +1,93 @@
+package com.svt.game;
+
+import com.badlogic.gdx.math.Rectangle;
+
+/**
+ * @author svt
+ *
+ */
+public class NthTile {
+	
+	int textureIndex;  /*Numero para determinar que textura pintar en el render. En la clase principal habrá un arreglo con todas las texturas para mejorar performance*/
+	float x; /*posicion en x en el sistema de coordenadas*/
+	float y; /*posicion en y en el sistema de coordenadas*/
+	float width; 
+	float height;
+	Rectangle bounds; /*Rectangulo con la forma del tile*/
+	int type=0; /*Tipo del tile*/
+	/*Diccionario de tipos*/
+	public final int ENABLED = 1;
+	public final int DISABLED = 3;
+	public final int WATER = 3;
+	public final int OCCUPIED = 3;
+	public final int TREE = 4;
+	public final int MOUNTAIN = 4;
+	/*--------------------*/
+	
+	public NthTile(int index,int x, int y, float width,float height,int type){
+		this.textureIndex = index;
+		this.x=x;
+		this.y=y;
+		this.width=width;
+		this.height=height;
+		this.type=type;
+		this.setBounds();
+	}
+	
+	
+	
+	public void setBounds(){
+		this.bounds = new Rectangle(this.x, this.y, this.width, this.height);
+	}
+		
+	
+	public int getTextureIndex() {
+		return textureIndex;
+	}
+
+	public void setTextureIndex(int textureIndex) {
+		this.textureIndex = textureIndex;
+	}
+
+	public float getX() {
+		return x;
+	}
+
+	public void setX(float x) {
+		this.x = x;
+	}
+
+	public float getY() {
+		return y;
+	}
+
+	public void setY(float y) {
+		this.y = y;
+	}
+
+	public float getWidth() {
+		return width;
+	}
+
+	public void setWidth(float width) {
+		this.width = width;
+	}
+
+	public float getHeight() {
+		return height;
+	}
+
+	public void setHeight(float height) {
+		this.height = height;
+	}
+
+	public int getType() {
+		return type;
+	}
+
+	public void setType(int type) {
+		this.type = type;
+	}
+	
+
+}
